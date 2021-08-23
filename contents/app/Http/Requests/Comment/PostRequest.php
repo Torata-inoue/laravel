4c4b7bbdfa0ececd8cde4b16805fc02bbe42ec08
tora <i_am_torao@yahoo.co.jp>
2021-08-23 22:54:34 +0900

    api作成

---
+++ contents/app/Http/Requests/Comment/PostRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Comment;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'user_id' => 'required|int',
+            'text' => 'required|string|max:10000',
+            'nominees' => 'required|array|min:1|max:10',
+            'nominees.*.user_id' => 'required|int',
+        ];
+    }
+}
